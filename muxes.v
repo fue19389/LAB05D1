5b57103c0958af532b37873c9a540cdb6013154f
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-16 01:08:19 -0600

    Todos los muxes funcionando

--- muxes.v
+++ muxes.v
@@ -21,51 +21,95 @@ endmodule
 //Modulo multiplexer 8:1 a partir de modulos 4:1 y 2:1
 module mux8(input [7:0]d2, input[2:0]s2, output[1:0]y22, y23, y24, output y2);
 
-  mux4([3:0]d2, s2[1:0], y23[1:0], y22[0]);
-  mux4([7:4]d2, s2[1:0], y24[1:0], y22[1]);
-  mux2(y22[1:0], [2]s2, y2);
+  mux4 m4(d2[3:0], s2[1:0], y23[1:0], y22[0]);
+  mux4 m5(d2[7:4], s2[1:0], y24[1:0], y22[1]);
+  mux2 m6(y22[1:0], s2[2], y2);
 
-//endmodule
+endmodule
+
+
+
+
+
+
+
 
 //Modulo Tabla 1 con mux 2:1
-module mux2_1(input [1:0]d0, input t, output [1:0]y2, output y3);
+module mux2_1(input [1:0]i0, input j, output [1:0]k11, output k1);
 
-    assign y2[0]= (d0[1] ^ d0[0]);
-    assign y2[1]= ~(d0[1] ^ d0[0]);
-    mux2 M3(y2[1:0], t, y3);
+    assign k11[0]= (i0[1] ^ i0[0]);
+    assign k11[1]= ~(i0[1] ^ i0[0]);
+    mux2 M3(k11[1:0], j, k1);
 
 endmodule
 
 //Modulo Tabla 2 con mux 2:1
-module mux2_2(input [1:0]d0, input t, output [1:0]y2, output y3);
+module mux2_2(input [1:0]i0, input j, output [1:0]k22, output k2);
 
-  assign y2[0]= ~(d0[1] | d0[0]);
-  assign y2[1]= ~(d0[1] & d0[0]);
-  mux2 M4(y2[1:0], t, y3);
+  assign k22[0]= ~(i0[1] | i0[0]);
+  assign k22[1]= ~(i0[1] & i0[0]);
+  mux2 M4(k22[1:0], j, k2);
 
 endmodule
 
-//Modulo Tabla 1 con mux 4:1
-module mux4_1(input[2:0]d1, output[1:0]y4_2, output[3:0]y4_1, output y5);
 
-  assign y4_1[0]= d1[0];
-  assign y4_1[1]= ~d1[0];
-  assign y4_1[2]= ~d1[0];
-  assign y4_1[3]= d1[0];
-  mux4 M5(y4_1[3:0], d1[2:1], y4_2[1:0], y5);
+
+
+
+//Modulo Tabla 1 con mux 4:1
+module mux4_1(input[2:0]n1, output[1:0]l111, output[3:0]l11, output l1);
+
+  assign l11[0]= n1[0];
+  assign l11[1]= ~n1[0];
+  assign l11[2]= ~n1[0];
+  assign l11[3]= n1[0];
+  mux4 M5(l11[3:0], n1[2:1], l111[1:0], l1);
 
 endmodule
 
 //Modulo Tabla 2 con mux 4:1
-module mux4_2(input[2:0]d1, output[1:0]y4_2, output[3:0]y4_1, output y5);
+module mux4_2(input[2:0]n1, output[1:0]l222, output[3:0]l22, output l2);
 
-  assign y4_1[0]= ~d1[0];
-  assign y4_1[1]= 0;
-  assign y4_1[2]= 1;
-  assign y4_1[3]= ~d1[0];
-  mux4 M5(y4_1[3:0], d1[2:1], y4_2[1:0], y5);
+  assign l22[0]= ~n1[0];
+  assign l22[1]= 0;
+  assign l22[2]= 1;
+  assign l22[3]= ~n1[0];
+  mux4 M5(l22[3:0], n1[2:1], l222[1:0], l2);
 
 endmodule
 
+
+
+
+
+
+
 //Modulo Tabla 1 con mux 8:1
-module mux8_1(input [7:0]d2, input[2:0]s2, output[1:0]y22, y23, y24, output y2);
+module mux8_1(output [7:0]v2, input[2:0]o2, output[1:0]p11, p12, p13, output p1);
+
+  assign v2[0]= 0;
+  assign v2[1]= 1;
+  assign v2[2]= 1;
+  assign v2[3]= 0;
+  assign v2[4]= 1;
+  assign v2[5]= 0;
+  assign v2[6]= 0;
+  assign v2[7]= 1;
+  mux8 M6(v2[7:0], o2[2:0], p11[1:0], p12[1:0], p13[1:0], p1);
+
+endmodule
+
+//Modulo Tabla 2 con mux 8:1
+module mux8_2(output [7:0]q2, input[2:0]o2, output[1:0]p21, p22, p23, output p2);
+
+  assign q2[0]= 1;
+  assign q2[1]= 0;
+  assign q2[2]= 0;
+  assign q2[3]= 0;
+  assign q2[4]= 1;
+  assign q2[5]= 1;
+  assign q2[6]= 1;
+  assign q2[7]= 0;
+  mux8 M7(q2[7:0], o2[2:0], p21[1:0], p22[1:0], p23[1:0], p2);
+
+endmodule
--- muxes_tb.v
+++ muxes_tb.v
@@ -4,24 +4,27 @@
 
 module testbench();
 
-  reg [2:0]d2, d3;
+  reg [2:0]d2, d3, d4, d6;
   reg [1:0]d0, d1;
   reg  t, u;
+  wire [7:0]d100, d200;
   wire [3:0]y100, y200;
-  wire [1:0]y2, y4, y6, y8;
-  wire y3, y5, y7, y9;
+  wire [1:0]y2, y4, y6, y8, y10, y12, y14, y16, y18, y20;
+  wire y3, y5, y7, y9, y11, y13;
 
 
   mux2_1 M2(d0, t, y2, y3);
   mux2_2 M3(d1, u, y4, y5);
   mux4_1 M4(d2, y6, y100, y7);
   mux4_2 M5(d3, y8, y200, y9);
+  mux8_1 M6(d100, d4, y10, y12, y14, y11);
+  mux8_2 M7(d200, d6, y16, y18, y20, y13);
 
   initial begin
   #1
     $display("\n");
-    $display("Todas las ecuaciones");
-    $display("t b c | y");
+    $display("TABLA NO1 MUX 2:1");
+    $display("A B C | Y");
     $display("------|--");
     $monitor("%b %b %b | %b", t, d0[1], d0[0], y3);
        t = 0; d0[1] = 0; d0[0] = 0;
@@ -37,8 +40,8 @@ module testbench();
   initial begin
   #9
     $display("\n");
-    $display("Todas las ecuaciones");
-    $display("t b c | y");
+    $display("TABLA NO2 MUX 2:1");
+    $display("A B C | Y");
     $display("------|--");
     $monitor("%b %b %b | %b", u, d1[1], d1[0], y5);
        u = 0; d1[1] = 0; d1[0] = 0;
@@ -85,8 +88,42 @@ module testbench();
     #1 d3[2] = 1; d3[1] = 1; d3[0] = 1;
   end
 
+  initial begin
+  #40
+    $display("\n");
+    $display("Todas las ecuaciones");
+    $display("t b c | y");
+    $display("------|--");
+    $monitor("%b %b %b | %b", d4[2], d4[1], d4[0], y11);
+       d4[2] = 0; d4[1] = 0; d4[0] = 0;
+    #1 d4[2] = 0; d4[1] = 0; d4[0] = 1;
+    #1 d4[2] = 0; d4[1] = 1; d4[0] = 0;
+    #1 d4[2] = 0; d4[1] = 1; d4[0] = 1;
+    #1 d4[2] = 1; d4[1] = 0; d4[0] = 0;
+    #1 d4[2] = 1; d4[1] = 0; d4[0] = 1;
+    #1 d4[2] = 1; d4[1] = 1; d4[0] = 0;
+    #1 d4[2] = 1; d4[1] = 1; d4[0] = 1;
+  end
+
+  initial begin
+  #55
+    $display("\n");
+    $display("Todas las ecuaciones");
+    $display("t b c | y");
+    $display("------|--");
+    $monitor("%b %b %b | %b", d6[2], d6[1], d6[0], y13);
+       d6[2] = 0; d6[1] = 0; d6[0] = 0;
+    #1 d6[2] = 0; d6[1] = 0; d6[0] = 1;
+    #1 d6[2] = 0; d6[1] = 1; d6[0] = 0;
+    #1 d6[2] = 0; d6[1] = 1; d6[0] = 1;
+    #1 d6[2] = 1; d6[1] = 0; d6[0] = 0;
+    #1 d6[2] = 1; d6[1] = 0; d6[0] = 1;
+    #1 d6[2] = 1; d6[1] = 1; d6[0] = 0;
+    #1 d6[2] = 1; d6[1] = 1; d6[0] = 1;
+  end
+
   initial
-    #80 $finish;
+    #120 $finish;
 
   initial begin
     $dumpfile("muxes_tb.vcd");
