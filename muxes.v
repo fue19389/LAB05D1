9c4300a452d968fe37cfdb3f51a483c5c1953594
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-15 21:03:10 -0600

    mux 2:1 funcional para ambas tablas

--- muxes.v
+++ muxes.v
@@ -2,13 +2,49 @@
 //Gerardo Fuentes
 //19389
 
+//Modulo multiplexer 2:1 genérico
 module mux2(input [1:0]d0, input s, output y);
 
   assign y = s ? d0[1]:d0[0];
 
 endmodule
 
-module mux4(input [3:0]d1, input[1:]s1, output y1)
+//Modulo multiplexer 4:1 a partir de un 2:1 genérico
+//module mux4(input [3:0]d1, input[1:0]s1, output y1);
 
-  mux2 m1();
-  mux2 m2();
+//  wire w1, w2;
+
+//  mux2 m1([1:0]d1, [0]s1, w1);
+//  mux2 m2([3:2]d1, [0]s1, w2);
+//  mux2 m3(w2, w1, [1]s1, y1);
+
+//endmodule
+
+//Modulo multiplexer 8:1 a partir de modulos 4:1 y 2:1
+//module mux8(input [7:0]d2, input[2:0]s2, output y2);
+
+//  wire w3, w4;
+
+//  mux4([3:0]d2, [1:0]s2, w3);
+//  mux4([7:4]d2, [1:0]s2, w4);
+//  mux2(w4, w3, [2]s2, y2);
+
+//endmodule
+
+//Modulo Tabla 1 con mux 2:1
+module mux2_1(input [1:0]d0, input t, output [1:0]y2, output y3);
+
+    assign y2[0]= (d0[1] ^ d0[0]);
+    assign y2[1]= ~(d0[1] ^ d0[0]);
+    mux2 M3(y2[1:0], t, y3);
+
+endmodule
+
+//Modulo Tabla 2 con mux 2:1
+module mux2_2(input [1:0]d1, input u, output [1:0]y4, output y5);
+
+  assign y4[0]= ~(d1[1] | d1[0]);
+  assign y4[1]= ~(d1[1] & d1[0]);
+  mux2 M4(y4[1:0], u, y5);
+
+endmodule
--- muxes_tb.v
+++ muxes_tb.v
@@ -4,31 +4,52 @@
 
 module testbench();
 
-  reg [1:0]d0;
-  reg s;
-  wire y;
+  reg [1:0]d0, d1;
+  reg  t, u;
+  wire [1:0]y2, y4;
+  wire y3, y5;
 
-  mux2 M1(d0, s, y);
+//  mux2 M1();
+  mux2_1 M2(d0, t, y2, y3);
+  mux2_2 M3(d1, u, y4, y5);
 
   initial begin
   #1
     $display("\n");
     $display("Todas las ecuaciones");
-    $display("s b c | y");
-    $display("------|---");
-    $monitor("%b %b %b | %b", s, d0[1], d0[0], y);
-       s = 0; d0[1] = 0; d0[0] = 0;
-    #1 s = 0; d0[1] = 0; d0[0] = 1;
-    #1 s = 0; d0[1] = 1; d0[0] = 0;
-    #1 s = 0; d0[1] = 1; d0[0] = 1;
-    #1 s = 1; d0[1] = 0; d0[0] = 0;
-    #1 s = 1; d0[1] = 0; d0[0] = 1;
-    #1 s = 1; d0[1] = 1; d0[0] = 0;
-    #1 s = 1; d0[1] = 1; d0[0] = 1;
+    $display("t b c | y");
+    $display("------|--");
+    $monitor("%b %b %b | %b", t, d0[1], d0[0], y3);
+       t = 0; d0[1] = 0; d0[0] = 0;
+    #1 t = 0; d0[1] = 0; d0[0] = 1;
+    #1 t = 0; d0[1] = 1; d0[0] = 0;
+    #1 t = 0; d0[1] = 1; d0[0] = 1;
+    #1 t = 1; d0[1] = 0; d0[0] = 0;
+    #1 t = 1; d0[1] = 0; d0[0] = 1;
+    #1 t = 1; d0[1] = 1; d0[0] = 0;
+    #1 t = 1; d0[1] = 1; d0[0] = 1;
   end
 
+  initial begin
+  #9
+    $display("\n");
+    $display("Todas las ecuaciones");
+    $display("t b c | y");
+    $display("------|--");
+    $monitor("%b %b %b | %b", u, d1[1], d1[0], y5);
+       u = 0; d1[1] = 0; d1[0] = 0;
+    #1 u = 0; d1[1] = 0; d1[0] = 1;
+    #1 u = 0; d1[1] = 1; d1[0] = 0;
+    #1 u = 0; d1[1] = 1; d1[0] = 1;
+    #1 u = 1; d1[1] = 0; d1[0] = 0;
+    #1 u = 1; d1[1] = 0; d1[0] = 1;
+    #1 u = 1; d1[1] = 1; d1[0] = 0;
+    #1 u = 1; d1[1] = 1; d1[0] = 1;
+  end
+
+
   initial
-    #20 $finish;
+    #40 $finish;
 
   initial begin
     $dumpfile("muxes_tb.vcd");
